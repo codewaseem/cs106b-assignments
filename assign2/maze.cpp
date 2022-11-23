4dfd46d4075b362567d5f58fea555727839228e1
codewaseem <codewaseem@gmail.com>
2022-11-23 21:50:00 +0530

    complete assignment-2 warmup section

---
+++ assign2/maze.cpp
@@ -0,0 +1,206 @@
+// TODO: remove and replace this file header comment
+// This is a .cpp file you will edit and turn in.
+// Remove starter comments and add your own
+// comments on each function and on complex code sections.
+#include <iostream>
+#include <fstream>
+#include "error.h"
+#include "filelib.h"
+#include "grid.h"
+#include "maze.h"
+#include "mazegraphics.h"
+#include "queue.h"
+#include "set.h"
+#include "stack.h"
+#include "vector.h"
+#include "testing/SimpleTest.h"
+using namespace std;
+
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+Set<GridLocation> generateValidMoves(Grid<bool>& maze, GridLocation cur) {
+    Set<GridLocation> neighbors;
+    /* TODO: Fill in the remainder of this function. */
+    return neighbors;
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+void validatePath(Grid<bool>& maze, Stack<GridLocation> path) {
+    GridLocation mazeExit = {maze.numRows()-1,  maze.numCols()-1};
+
+    if (path.peek() != mazeExit) {
+        error("Path does not end at maze exit");
+    }
+    /* TODO: Fill in the remainder of this function. */
+
+    /* If you find a problem with the path, call error() to report it.
+     * If the path is a valid solution, then this function should run to completion
+     * without throwing any errors.
+     */
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+Stack<GridLocation> solveMaze(Grid<bool>& maze) {
+    MazeGraphics::drawGrid(maze);
+    Stack<GridLocation> path;
+    /* TODO: Fill in the remainder of this function. */
+    return path;
+}
+
+/*
+ * The given readMazeFile function correctly reads a well-formed
+ * maze from a file.
+ *
+ * This provided function is fully correct. You do not need to change
+ * any of the code in this function.
+ */
+void readMazeFile(string filename, Grid<bool>& maze) {
+    /* The following code reads data from the file into a Vector
+     * of strings representing the lines of the file.
+     */
+    ifstream in;
+
+    if (!openFile(in, filename))
+        error("Cannot open file named " + filename);
+
+    Vector<string> lines;
+    readEntireFile(in, lines);
+
+    /* Now that the file data has been read into the Vector, populate
+     * the maze grid.
+     */
+    int numRows = lines.size();        // rows is count of lines
+    int numCols = lines[0].length();   // cols is length of line
+    maze.resize(numRows, numCols);     // resize grid dimensions
+
+    for (int r = 0; r < numRows; r++) {
+        if (lines[r].length() != numCols) {
+            error("Maze row has inconsistent number of columns");
+        }
+        for (int c = 0; c < numCols; c++) {
+            char ch = lines[r][c];
+            if (ch == '@') {        // wall
+                maze[r][c] = false;
+            } else if (ch == '-') { // corridor
+                maze[r][c] = true;
+            } else {
+                error("Maze location has invalid character: '" + charToString(ch) + "'");
+            }
+        }
+    }
+}
+
+/* The given readSolutionFile correctly reads a path from a file.
+ *
+ * This provided function is fully correct. You do not need to change
+ * any of the code in this function.
+ */
+void readSolutionFile(string filename, Stack<GridLocation>& soln) {
+    ifstream in;
+
+    if (!openFile(in, filename)) {
+        error("Cannot open file named " + filename);
+    }
+
+    if (!(in >> soln)) {// if not successfully read
+        error("Maze solution did not have the correct format.");
+    }
+}
+
+
+/* * * * * * Test Cases * * * * * */
+
+PROVIDED_TEST("generateValidMoves on location in the center of 3x3 grid with no walls") {
+    Grid<bool> maze = {{true, true, true},
+                       {true, true, true},
+                       {true, true, true}};
+    GridLocation center = {1, 1};
+    Set<GridLocation> expected = {{0, 1}, {1, 0}, {1, 2}, {2, 1}};
+
+    EXPECT_EQUAL(generateValidMoves(maze, center), expected);
+}
+
+PROVIDED_TEST("generateValidMoves on location on the side of 3x3 grid with no walls") {
+    Grid<bool> maze = {{true, true, true},
+                       {true, true, true},
+                       {true, true, true}};
+    GridLocation side = {0, 1};
+    Set<GridLocation> expected = {{0,0}, {0,2}, {1, 1}};
+
+    EXPECT_EQUAL(generateValidMoves(maze, side), expected);
+}
+
+PROVIDED_TEST("generateValidMoves on corner of 2x2 grid with walls") {
+    Grid<bool> maze = {{true, false},
+                       {true, true}};
+    GridLocation corner = {0, 0};
+    Set<GridLocation> expected = {{1, 0}};
+
+    EXPECT_EQUAL(generateValidMoves(maze, corner), expected);
+}
+
+PROVIDED_TEST("validatePath on correct solution") {
+    Grid<bool> maze = {{true, false},
+                       {true, true}};
+    Stack<GridLocation> soln = { {0 ,0}, {1, 0}, {1, 1} };
+
+    EXPECT_NO_ERROR(validatePath(maze, soln));
+}
+
+PROVIDED_TEST("validatePath on correct solution loaded from file for medium maze") {
+    Grid<bool> maze;
+    Stack<GridLocation> soln;
+    readMazeFile("res/5x7.maze", maze);
+    readSolutionFile("res/5x7.soln", soln);
+
+    EXPECT_NO_ERROR(validatePath(maze, soln));
+}
+
+PROVIDED_TEST("validatePath on correct solution loaded from file for large maze") {
+    Grid<bool> maze;
+    Stack<GridLocation> soln;
+    readMazeFile("res/25x33.maze", maze);
+    readSolutionFile("res/25x33.soln", soln);
+
+    EXPECT_NO_ERROR(validatePath(maze, soln));
+}
+
+PROVIDED_TEST("validatePath on invalid path should raise error") {
+    Grid<bool> maze = {{true, false},
+                       {true, true}};
+    Stack<GridLocation> not_end_at_exit = { {1, 0}, {0, 0} };
+    Stack<GridLocation> not_begin_at_entry = { {1, 0}, {1, 1} };
+    Stack<GridLocation> go_through_wall = { {0 ,0}, {0, 1}, {1, 1} };
+    Stack<GridLocation> teleport = { {0 ,0}, {1, 1} };
+    Stack<GridLocation> revisit = { {0 ,0}, {1, 0}, {0, 0}, {1, 0}, {1, 1} };
+
+    EXPECT_ERROR(validatePath(maze, not_end_at_exit));
+    EXPECT_ERROR(validatePath(maze, not_begin_at_entry));
+    EXPECT_ERROR(validatePath(maze, go_through_wall));
+    EXPECT_ERROR(validatePath(maze, teleport));
+    EXPECT_ERROR(validatePath(maze, revisit));
+}
+
+PROVIDED_TEST("solveMaze on file 5x7") {
+    Grid<bool> maze;
+    readMazeFile("res/5x7.maze", maze);
+    Stack<GridLocation> soln = solveMaze(maze);
+
+    EXPECT_NO_ERROR(validatePath(maze, soln));
+}
+
+PROVIDED_TEST("solveMaze on file 21x23") {
+    Grid<bool> maze;
+    readMazeFile("res/21x23.maze", maze);
+    Stack<GridLocation> soln = solveMaze(maze);
+
+    EXPECT_NO_ERROR(validatePath(maze, soln));
+}
+
+// TODO: add your test cases here
---
+++ assign2/search.cpp
@@ -0,0 +1,121 @@
+// TODO: remove and replace this file header comment
+// This is a .cpp file you will edit and turn in.
+// Remove starter comments and add your own
+// comments on each function and on complex code sections.
+#include <iostream>
+#include <fstream>
+#include "error.h"
+#include "filelib.h"
+#include "map.h"
+#include "search.h"
+#include "set.h"
+#include "strlib.h"
+#include "vector.h"
+#include "testing/SimpleTest.h"
+using namespace std;
+
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+string cleanToken(string s)
+{
+    return s;
+}
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+Set<string> gatherTokens(string text)
+{
+    Set<string> tokens;
+    return tokens;
+}
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+int buildIndex(string dbfile, Map<string, Set<string>>& index)
+{
+    return 0;
+}
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+Set<string> findQueryMatches(Map<string, Set<string>>& index, string query)
+{
+    Set<string> result;
+    // TODO: your code here
+    return result;
+}
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+void searchEngine(string dbfile)
+{
+    // TODO: your code here
+}
+
+/* * * * * * Test Cases * * * * * */
+
+PROVIDED_TEST("cleanToken on strings with no punctuation at beginning or end") {
+    EXPECT_EQUAL(cleanToken("hello"), "hello");
+    EXPECT_EQUAL(cleanToken("WORLD"), "world");
+    EXPECT_EQUAL(cleanToken("CS*106B"), "cs*106b");
+}
+
+PROVIDED_TEST("cleanToken on strings with some punctuation at beginning and end") {
+    EXPECT_EQUAL(cleanToken("/hello/"), "hello");
+    EXPECT_EQUAL(cleanToken("~woRLD!"), "world");
+}
+
+PROVIDED_TEST("cleanToken on non-word strings (no letters)"){
+    EXPECT_EQUAL(cleanToken("106"), "");
+    EXPECT_EQUAL(cleanToken("~!106!!!"), "");
+}
+
+PROVIDED_TEST("gatherTokens from simple string") {
+    Set<string> expected = {"go", "gophers"};
+    EXPECT_EQUAL(gatherTokens("go go go gophers"), expected);
+}
+
+PROVIDED_TEST("gatherTokens correctly cleans tokens") {
+    Set<string> expected = {"i", "love", "cs*106b"};
+    EXPECT_EQUAL(gatherTokens("I _love_ CS*106B!"), expected);
+}
+
+PROVIDED_TEST("gatherTokens from seuss, 5 unique words, mixed case, punctuation") {
+    Set<string> tokens = gatherTokens("One Fish Two Fish *Red* fish Blue fish ** 10 RED Fish?");
+    EXPECT_EQUAL(tokens.size(), 5);
+    EXPECT(tokens.contains("fish"));
+    EXPECT(!tokens.contains("Fish"));
+}
+
+PROVIDED_TEST("buildIndex from tiny.txt, 4 pages, 11 unique tokens") {
+    Map<string, Set<string>> index;
+    int nPages = buildIndex("res/tiny.txt", index);
+    EXPECT_EQUAL(nPages, 4);
+    EXPECT_EQUAL(index.size(), 11);
+    EXPECT(index.containsKey("fish"));
+}
+
+PROVIDED_TEST("findQueryMatches from tiny.txt, single word query") {
+    Map<string, Set<string>> index;
+    buildIndex("res/tiny.txt", index);
+    Set<string> matchesRed = findQueryMatches(index, "red");
+    EXPECT_EQUAL(matchesRed.size(), 2);
+    EXPECT(matchesRed.contains("www.dr.seuss.net"));
+    Set<string> matchesHippo = findQueryMatches(index, "hippo");
+    EXPECT(matchesHippo.isEmpty());
+}
+
+PROVIDED_TEST("findQueryMatches from tiny.txt, compound queries") {
+    Map<string, Set<string>> index;
+    buildIndex("res/tiny.txt", index);
+    Set<string> matchesRedOrFish = findQueryMatches(index, "red fish");
+    EXPECT_EQUAL(matchesRedOrFish.size(), 4);
+    Set<string> matchesRedAndFish = findQueryMatches(index, "red +fish");
+    EXPECT_EQUAL(matchesRedAndFish.size(), 1);
+    Set<string> matchesRedWithoutFish = findQueryMatches(index, "red -fish");
+    EXPECT_EQUAL(matchesRedWithoutFish.size(), 1);
+}
+
+
+// TODO: add your test cases here
